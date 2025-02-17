027a83c4e58caf4cdd9255b41e0c14262d5fa5ec
Eduardo <educlanmariano@gmail.com>
2025-02-17 10:35:35 -0400

    adicionado atividades e exemplos de condicionais

---
+++ exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿int idade = 17;
+bool autorizacao = false;
+
+if (idade < 18 )
+{
+    if(autorizacao)
+    {
+        Console.WriteLine($"entrada liberada");
+    }
+    else
+    {
+        Console.WriteLine("entrada não permitida");
+    }
+}
+else if (idade >= 18 )
+{
+    Console.WriteLine($"entrada liberada");
+}
