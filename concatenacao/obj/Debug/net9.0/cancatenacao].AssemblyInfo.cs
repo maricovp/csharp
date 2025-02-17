50f4bd706fc0f8191b10916671627d166a003a71
Eduardo <educlanmariano@gmail.com>
2025-02-17 10:49:06 -0400

    adicionado atividades e exemplos de condicionais

--- concatenacao/obj/Debug/net9.0/cancatenacao].AssemblyInfo.cs
+++ concatenacao/obj/Debug/net9.0/cancatenacao].AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cancatenacao]")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb068d99b40a600cf64f6af7bbc5887334c96d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f627938d2a3d6a458a6d27df69690ebbded95182")]
 [assembly: System.Reflection.AssemblyProductAttribute("cancatenacao]")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cancatenacao]")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- exercicio1/Program.cs
+++ exercicio1/Program.cs
@@ -16,3 +16,23 @@ else if (idade >= 18 )
 {
     Console.WriteLine($"entrada liberada");
 }
+
+//atividade da presença minima//
+int presenca = 7;
+bool  presencaminima = false;
+
+if (presenca <= 7 )
+{
+    if(presencaminima)
+    {
+        Console.WriteLine($"aprovado");
+    }
+    else
+    {
+        Console.WriteLine("entrada não permitida");
+    }
+}
+else if (idade >= 7)
+{
+    Console.WriteLine($"entrada liberada");
+}
