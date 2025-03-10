83eb108c01f417c33ceccb4efe5e376f3a4f4dfb
Eduardo <marcos2015vinicuis@gmail.com>
2025-03-10 11:30:30 -0400

    interminado

---
+++ POO/Program.cs
@@ -0,0 +1,7 @@
+﻿
+﻿using System;
+using POO.modelo;
+
+Veiculos obj = new Veiculos ("kia", "Picanto", 2023, 550);
+obj.Acelerar();
+obj.Freiar();
---
+++ POO/modelo/Veiculo.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO.modelo
+{
+
+public class Veiculos
+{
+ // Atributos
+private string marca;
+private string modelo;
+ private int ano;
+protected double velocidadeatual;
+
+         // Construtor
+public Veiculos(string marca, string modelo, int ano, double velocidadeatual)
+{
+this.marca = marca;
+this.modelo = modelo;
+this.ano = ano;
+this.velocidadeatual= velocidadeatual;
+}
+public string GetMarca()
+{
+    return marca;
+}
+public void SetMarca(string marca)
+{
+    this.marca = marca;
+}
+public string GetModelo()
+{
+    return modelo;
+}
+public void SetModelo(string modelo)
+{
+    this.marca = modelo;
+}
+public int Getano()
+{
+    return ano;
+}
+public void Setano(int ano)
+{
+    this.ano = ano;
+}
+public double Getvelocidadeatual()
+{
+    return velocidadeatual;
+}
+public void Setvelocidadeatual(string marca)
+{
+    this.marca = marca;
+}
+
+ // Métodos Andar e Falar
+public void Acelerar()
+{
+Console.WriteLine($"{marca} {modelo} {ano} {velocidadeatual} está acelerando.");
+}
+public void Freiar(){
+Console.WriteLine($"{marca} {modelo} {ano} {velocidadeatual} está freiando");
+}
+}
+}
+    
