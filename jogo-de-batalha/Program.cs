0d5fccf54517d150559d4426ef81c794a59fd6a6
maricovp <marcos2015vinicuis@gmail.com>
2025-03-19 10:50:47 -0400

    fix bugs

--- jogo-de-batalha/Program.cs
+++ jogo-de-batalha/Program.cs
@@ -8,11 +8,9 @@ class Program
         // Criando dois personagens
         Personagem jogador1 = new Personagem("Guerreiro", 100, 20);
         Personagem jogador2 = new Personagem("Mago", 80, 25);
-
         // Exibindo o status inicial
         jogador1.ExibirStatus();
         jogador2.ExibirStatus();
-
         // Iniciando o sistema de turnos
         bool jogoAtivo = true;
         while (jogoAtivo)
@@ -26,26 +24,22 @@ class Program
                 jogoAtivo = false;
                 break;
             }
-
             // Exibindo o status após o turno
             jogador1.ExibirStatus();
             jogador2.ExibirStatus();
-
             // Turno do jogador 2
             Console.WriteLine("\nTurno do Mago!");
             jogador2.Atacar(jogador1);
             if (jogador1.Hp == 0)
             {
-                Console.WriteLine($"{jogador1.Nome} foi derrotado! {jogador2.Nome} venceu!");
+                Console.WriteLine($"{jogador1.Nome} foi derrotado!{jogador2.Nome} venceu!");
                 jogoAtivo = false;
                 break;
             }
-
             // Exibindo o status após o turno
             jogador1.ExibirStatus();
             jogador2.ExibirStatus();
         }
-
         Console.WriteLine("Fim do jogo!");
     }
 }
